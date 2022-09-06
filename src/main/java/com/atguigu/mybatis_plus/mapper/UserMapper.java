02765fd793ee1a1eda45f09d2ebc5384651b686e
Summerpepe <1942693104@qq.com>
2022-09-06 23:19:28 +0800

    start now

---
+++ src/main/java/com/atguigu/mybatis_plus/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.mybatis_plus.mapper;
+
+import com.atguigu.mybatis_plus.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * @Author Summerpepe
+ * @Date 2022-09-06
+ * @Version 1.0 
+ */
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+
+}
--- src/test/java/com/atguigu/mybatis_plus/MybatisPlusApplicationTests.java
+++ src/test/java/com/atguigu/mybatis_plus/MybatisPlusApplicationTests.java
@@ -1,14 +1,21 @@
 package com.atguigu.mybatis_plus;
 
+import com.atguigu.mybatis_plus.entity.User;
+import com.atguigu.mybatis_plus.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MybatisPlusApplicationTests {
-
+@Autowired
+   private UserMapper userMapper;
     @Test
     void contextLoads() {
-
+     List<User> users= userMapper.selectList(null);
+      users.forEach(System.out::println);
     }
 
 }
